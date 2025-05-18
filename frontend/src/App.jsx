87d3c0f9cabd66b9c735153ef92974c640852e64
ShwethaGayathri <shwetha4268@gmail.com>
2025-05-18 13:01:48 +0530

    Added conversation structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,32 +3,59 @@ import React,{useState} from 'react';
 import axios from "axios"
 
 function App() {
-  const [text,setText] = useState("");
-  const [targetLang,setTargetLang] = useState("");
-  const[translated,setTranslated] = useState("");
+  const [input,setInput] = useState("");
+  const [targetLang,setTargetLang] = useState("fr");
+  const[messages,setMessages] = useState(["BabelMate 😏: Hello! I’m your translation companion — ready to help! ✨"]);
+  const [languageLabel,setLanguageLabel] = useState("");
+
+  const languageMap = {
+    fr : 'French',
+    de: 'German',
+    hi: 'Hindi',
+    es: 'Spanish',
+    zh: 'Chinese',
+    ar: 'Arabic',
+    jap: 'Japanese',
+    vi: 'Vietnamese',
+
+  }
 
   const handleTranslate = async() =>{
+    if(!input.trim()) return;
+
+    const userMessage = `You 🥰: ${input}`;
+    setMessages((prev) => [...prev,userMessage])
     try{
       const res =  await axios.post("http://localhost:8000/translate",{
-        text,
+        text: input,
         target_lang: targetLang,
         model: "huggingface",
       });
-      setTranslated(res.data.translated_text);
+      const langLabel = languageMap[targetLang];
+      setLanguageLabel(langLabel);
+      const translatedText = res.data.translated_text;
+      const botMessage = `BabelMate 😏 [Translated to ${langLabel}] : ${translatedText}`
+      setMessages((prev) => [...prev, botMessage]);
     }catch(error){
-      console.error(error);
+      setMessages((prev) => [...prev,"BabelMate 😏: Failed To Translate."])
     }
+
+    setInput("")
   };
 
   return (
-    <div className='app'>
-      <h2>BabelMate : A Translator Chatbot</h2>
-      <textarea 
-      placeholder='Enter Text'
-      value= {text}
-      onChange={(e) => setText(e.target.value)}
-      />
-      <select value={targetLang} onChange={(e) =>setTargetLang(e.target.value)}>
+    <div className='app-container'>
+      <h2 className='heading'>BabelMate : A Translator Chatbot</h2>
+       <div className='chat-window'>
+        {messages.map((msg,idx) => (
+          <p key = {idx} className={msg.startsWith("You") ? "user-msg" :"bot-msg"}>
+            {msg}
+          </p>
+        ))}
+       </div>
+       <div className='controls'>
+        <input type='text' value={input} placeholder='Type your Message...' onChange={(e) => setInput(e.target.value)} />
+        <select value={targetLang} onChange={(e) =>setTargetLang(e.target.value)}>
         <option value="fr">French</option>
         <option value="de">German</option>
         <option value="es">Spanish</option>
@@ -40,9 +67,7 @@ function App() {
         <option value="vi">Vietnamese</option>
       </select>
       <button onClick={handleTranslate}>Translate</button>
-      <div className='output'>
-        <strong>Translation:</strong> {translated}
-      </div>
+     </div>
     </div>
   );
 
